49eeeb4ef001bb5a59f9946b5e095749832c541b
Tirupathi <tirupathit@gmail.com>
2025-09-05 17:49:44 +0530

    First.java

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=2;
+        b=5;
+        c=a+b;
+        System.out.println("Sum of "+ a+" and "+b +" is: "+c);
+    }
+
+}
